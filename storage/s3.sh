889f955bccdfa1eccae2776ac46ebad84fc49142
SergeyCherepanov <s@cherepanov.co>
2016-11-28 18:55:40 +0100

    s3 fix

--- storage/s3.sh
+++ storage/s3.sh
@@ -10,7 +10,7 @@ fi
 S3_ACCESS_KEY_ESCAPED=`echo "$S3_ACCESS_KEY" | sed -e 's/[\.\:\/&]/\\\\&/g'`
 S3_SECRET_KEY_ESCAPED=`echo "$S3_SECRET_KEY" | sed -e 's/[\.\:\/&]/\\\\&/g'`
 
-S3CFG=$(cat ${DIR}.s3cfg);
+S3CFG=$(cat ${DIR}/.s3cfg);
 S3CFG=$(echo "$S3CFG" | sed -e "s/access_key =.*/access_key = ${S3_ACCESS_KEY_ESCAPED}/g")
 S3CFG=$(echo "$S3CFG" | sed -e "s/secret_key =.*/secret_key = ${S3_SECRET_KEY_ESCAPED}/g")
 S3CFG=$(echo "$S3CFG" | sed -e "s/bucket_location =.*/bucket_location = ${S3_REGION}/g")
@@ -18,21 +18,21 @@ S3CFG=$(echo "$S3CFG" | sed -e "s/bucket_location =.*/bucket_location = ${S3_REG
 case $1 in
   "ls")
     PREFIX="s3://${S3_BUCKET}/";
-    s3cmd -c <(echo ${S3CFG}) ls -l ${PREFIX}$2 | awk '{ if ($6) { path = substr($6, '${#PREFIX}'); print $1 " " $2 " " $3 " " path " " $4 } else { path = substr($2, '${#PREFIX}'); print "- - " $1 " " path " " $4 }}'
+    s3cmd -c <(echo "${S3CFG}") ls -l ${PREFIX}$2 | awk '{ if ($6) { path = substr($6, '${#PREFIX}'); print $1 " " $2 " " $3 " " path " " $4 } else { path = substr($2, '${#PREFIX}'); print "- - " $1 " " path " " $4 }}'
     exit 0
   ;;
   "cp")
     if [[ -z $2 ]] || [[ -z $3 ]]; then
       echo "Missing argument "
     fi
-    s3cmd -c <(echo ${S3CFG}) put $2 s3://${S3_BUCKET}/$3
+    s3cmd -c <(echo "${S3CFG}") put $2 s3://${S3_BUCKET}/$3
     exit 0
   ;;
   "del")
     if [[ -z $2 ]] || [[ -z $3 ]]; then
       echo "Missing argument "
     fi
-    s3cmd -c <(echo ${S3CFG}) del s3://${S3_BUCKET}/$2
+    s3cmd -c <(echo "${S3CFG}") del s3://${S3_BUCKET}/$2
     exit 0
   ;;
 esac
